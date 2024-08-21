949b8429d202a819fd18dd43169acef19faa2b7b
Dev-Asfix <rosasramirez753@gmail.com>
2024-08-21 16:35:52 -0500

    Commit eventos

---
+++ Practicas/Ejercicios/mokepon/js/mokepon.js
@@ -0,0 +1,7 @@
+function seleccionarMascotaJugador(){
+    alert('Seleccionaste tu mascota')
+}
+
+let botonMascotaJugador = document.getElementById('btn-mascota');
+
+botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador())
