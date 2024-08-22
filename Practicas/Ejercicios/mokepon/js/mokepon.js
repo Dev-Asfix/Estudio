3b5d4b18048c4dd09a63ad2097067f67df2b41eb
Dev-Asfix <rosasramirez753@gmail.com>
2024-08-22 18:56:42 -0500

    Funcion aleatoria

--- Practicas/Ejercicios/mokepon/js/mokepon.js
+++ Practicas/Ejercicios/mokepon/js/mokepon.js
@@ -1,3 +1,6 @@
+let ataqueJugador;
+
+
 function iniciarJuego(){
     let botonMascotaJugador = document.getElementById('btn-mascotas');
 
@@ -9,21 +12,41 @@ function seleccionarMascotaJugador(){
     let inputHipodoge = document.getElementById('hipodoge');
     let inputCapipepo = document.getElementById('capipepo');
     let inputRatigueya = document.getElementById('ratigueya')
+    let spanMascotaJugador = document.getElementById('mascota-jugador')
 
     if(inputHipodoge.checked){
-        alert('Seleccionaste tu mascota Hipodoge')
+        spanMascotaJugador.innerHTML = "Hipodoge";
     } else if(inputCapipepo.checked){
-        alert('Seleccionaste tu mascota Capipepo')
+        spanMascotaJugador.innerHTML = "Capipepo";
     } else if(inputRatigueya.checked){
-        alert('Seleccionaste tu mascota Ratigueya')
+        spanMascotaJugador.innerHTML = "Ratigueya"
     } else {
         alert('Selecciona una mascota');
     }
 
+    seleccionarMascotaEnemigo();
+}
 
-    
+function seleccionarMascotaEnemigo(){
+
+    let   = document.getElementById('mascota-enemiga');
+    let enemigoAleatorio = numeroAleatorio(1,3);
+
+    if(enemigoAleatorio == 1){
+        spanMascotaEnemiga.innerHTML="Hipodoge";
+    } else if(enemigoAleatorio == 2){
+        spanMascotaEnemiga.innerHTML="Capipepo";
+    } else if(enemigoAleatorio){
+        spanMascotaEnemiga.innerHTML="Ratigueya";
+    } else {
+        spanMascotaEnemiga.innerHTML="Error"
+    }
+
 }
 
 
+function numeroAleatorio(min , max){
+    return Math.floor(Math.random()*(max-min+1)+min);
+}
 
 window.addEventListener('load',iniciarJuego)
