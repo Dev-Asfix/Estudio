c27f8dc9584f5c9a9f0d9c49fb44c02b9eba37cf
Dev-Asfix <rosasramirez753@gmail.com>
2024-10-04 20:43:12 -0500

    metodos

---
+++ Java/Area.java
@@ -0,0 +1,22 @@
+public class Area {
+
+    public double calcularArea(double radio){
+        return Math.PI * (radio * radio);
+    }
+
+    public double calcularArea(double largo, double ancho){
+
+        return largo * ancho;
+    }
+
+    public double calcularArea(double base ,double altura,boolean esTriangulo){
+        return (base*altura)/2;
+    }
+
+    public static void main(String args[]){
+        Area area = new Area();
+        System.out.println("El area de un circulo es : " + area.calcularArea(10));
+        System.out.println("El area de un rectangulo es : " + area.calcularArea(10,10));
+        System.out.println("El area de un triengulo es : " + area.calcularArea(10,5,false));
+    }
+}
