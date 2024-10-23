8cd4481a19fe2a3779f154ac2fe06b7ffda0d585
Dev-Asfix <rosasramirez753@gmail.com>
2024-10-23 18:14:58 -0500

    Clase compra

--- Java/com/arraylist/Compra.java
+++ Java/com/arraylist/Compra.java
@@ -1,5 +1,19 @@
 package com.arraylist;
 
+import java.util.Scanner;
+
 public class Compra {
-    
+    public double precio;
+    public String compra;
+    Scanner in = new Scanner(System.in);
+
+    public Compra(String compra, double precio){
+        this.compra = compra;
+        this.precio = precio;
+    }
+
+    @Override
+    public String toString(){
+        return "compra : "+ compra+ " precio: S/" +precio;
+    }
 }
