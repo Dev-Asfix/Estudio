88e2951e77381f0b02d03390e114cbeb0fbc0ea1
Dev-Asfix <rosasramirez753@gmail.com>
2024-10-23 18:15:48 -0500

    Creacion Clase Operacioin

--- Java/com/arraylist/Operacion.java
+++ Java/com/arraylist/Operacion.java
@@ -1,5 +1,56 @@
 package com.arraylist;
 
+
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
 public class Operacion {
-    
+
+    public int opcion = 0;
+    Scanner in = new Scanner(System.in);
+    ArrayList<Compra> lista = new ArrayList<>();
+
+    public void añadir(){
+        System.out.print("Nombre : ");
+        String compra = in.nextLine();
+        System.out.print("Precio:");
+        double precio = in.nextDouble();
+        in.nextLine();
+        lista.add(new Compra(compra,precio));
+    }
+
+    public void mostrar(){
+        System.out.println("Lista de Compras");
+        for(Compra list : lista){
+            System.out.println(list);
+        }
+    }
+
+    public void elegir(){
+        System.out.print("Elige una opcion: ");
+        opcion = in.nextInt();
+        in.nextLine();
+    }
+
+    public void bucle(){
+
+
+        while(opcion != 3){
+            elegir();
+            if(opcion == 1){
+                añadir();
+
+            } else if(opcion == 2){
+                mostrar();
+                break;
+            } else if(opcion == 3){
+                System.out.println("Saliendo del Programa");
+            } else {
+                System.out.println("ELige un numero correcto");
+            }
+        }
+    }
+
 }
