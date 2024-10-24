fb5a612c322b8153e42292871f9cb2c14d419d65
Dev-Asfix <rosasramirez753@gmail.com>
2024-10-23 20:52:43 -0500

    Main

--- Java/com/arraylist/Main.java
+++ Java/com/arraylist/Main.java
@@ -1,19 +1,14 @@
 package com.arraylist;
 
 
-import java.util.ArrayList;
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String args[]){
         Operacion in = new Operacion();
 
-        Scanner en = new Scanner(System.in);
-
-        System.out.println("Numero : ");
-        int as = en.nextInt();
-        System.out.println("Escribe : ");
-        int hola = en.nextInt();
+        in.bucle();
 
     }
 }
