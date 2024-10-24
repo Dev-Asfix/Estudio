28b9b30420d2015ffab73ec5bf0e9e8350794401
Dev-Asfix <rosasramirez753@gmail.com>
2024-10-23 22:17:17 -0500

    Buenas practicas

--- Java/com/arraylist/Compra.java
+++ Java/com/arraylist/Compra.java
@@ -1,26 +1,27 @@
 package com.arraylist;
 
-import java.util.Scanner;
-
 public  class Compra implements Comparable<Compra>{
-    public double precio;
-    public String compra;
-    Scanner in = new Scanner(System.in);
+    private double precio;
+    private String compra;
 
+    // Contructor
     public Compra(String compra, double precio){
         this.compra = compra;
         this.precio = precio;
     }
 
+    // Getters
     public double getPrecio() {
         return precio;
     }
 
+    //Sobrescribit toString para representar correctamente el onjeto Compra
     @Override
     public String toString(){
-        return "compra : "+ compra+ " precio: S/" +precio;
+        return String.format("Compra: %s | Precio: S/ %.2f", compra,precio);
     }
 
+    //Comparar por nombre
     @Override
     public int compareTo(Compra otraCompra) {
         return this.compra.compareTo(otraCompra.compra);
