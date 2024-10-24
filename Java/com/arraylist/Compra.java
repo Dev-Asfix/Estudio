eac6bc4e2ee24271056fa872c8af095108ec7aa2
Dev-Asfix <rosasramirez753@gmail.com>
2024-10-23 21:58:02 -0500

    Agregamos un implemento

--- Java/com/arraylist/Compra.java
+++ Java/com/arraylist/Compra.java
@@ -2,7 +2,7 @@ package com.arraylist;
 
 import java.util.Scanner;
 
-public class Compra {
+public  class Compra implements Comparable<Compra>{
     public double precio;
     public String compra;
     Scanner in = new Scanner(System.in);
@@ -12,8 +12,21 @@ public class Compra {
         this.precio = precio;
     }
 
+    public double getPrecio() {
+        return precio;
+    }
+
     @Override
     public String toString(){
         return "compra : "+ compra+ " precio: S/" +precio;
+    }
+
+    @Override
+    public int compareTo(Compra otraCompra) {
+        return this.compra.compareTo(otraCompra.compra);
+    }
+
+    public static int compareByPrecio(Compra c1 , Compra c2){
+        return Double.compare(c2.precio, c1.precio);
     }
 }
