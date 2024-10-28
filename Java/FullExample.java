00b621e7100e923dff5557c76958f48c023b926f
Dev-Asfix <rosasramirez753@gmail.com>
2024-10-27 23:54:23 -0500

    request

--- Java/FullExample.java
+++ Java/FullExample.java
@@ -1,6 +1,6 @@
 import java.net.URI;
 import java.net.http.HttpClient;//Protocolo HTTP
-import java.net.http.HttpRequest;
+import java.net.http.HttpRequest; //Protocolo REquest
 import java.net.http.HttpResponse;
 import java.util.Scanner;
 
