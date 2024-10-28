a6f7767f62ba066b9fa1d9f5d5941943c7e79b35
Dev-Asfix <rosasramirez753@gmail.com>
2024-10-27 23:53:20 -0500

    import http

--- Java/FullExample.java
+++ Java/FullExample.java
@@ -1,5 +1,5 @@
 import java.net.URI;
-import java.net.http.HttpClient;
+import java.net.http.HttpClient;//Protocolo HTTP
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.Scanner;
