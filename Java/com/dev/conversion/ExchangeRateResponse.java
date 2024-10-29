0e779fa1d40c61f60b8ba4b150e7640a8ae4371e
Dev-Asfix <rosasramirez753@gmail.com>
2024-10-29 12:51:15 -0500

    Response

--- Java/com/dev/conversion/ExchangeRateResponse.java
+++ Java/com/dev/conversion/ExchangeRateResponse.java
@@ -1,5 +1,18 @@
 package com.dev.conversion;
+package com.dev.converion;
 
-public class ExchangeRateResponse {
-    
-}
+import com.google.gson.annotations.SerializedName;
+
+public record ExchangeRateResponse(
+        @SerializedName("result") String resultado,
+        @SerializedName("documentation") String documentacion,
+        @SerializedName("terms_of_use") String terminosDeUso,
+        @SerializedName("time_last_update_unix") long tiempoUltimaActualizacionUnix,
+        @SerializedName("time_last_update_utc") String tiempoUltimaActualizacionUtc,
+        @SerializedName("time_next_update_unix") long tiempoProximaActualizacionUnix,
+        @SerializedName("time_next_update_utc") String tiempoProximaActualizacionUtc,
+        @SerializedName("base_code") String codigoBase,
+        @SerializedName("target_code") String codigoObjetivo,
+        @SerializedName("conversion_rate") double tasaDeConversion,
+        @SerializedName("conversion_result") double resultadoConversion
+) {}
