f28325c8c88338bc60e46836420f15b603462dfe
Dev-Asfix <rosasramirez753@gmail.com>
2024-10-28 21:18:01 -0500

    Clase principal

---
+++ Java/com/dev/conversor/Cliente.java
@@ -0,0 +1,34 @@
+package com.dev.conversor;
+
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class Cliente {
+
+    public static void main(String []args){
+
+        try{
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create("https://v6.exchangerate-api.com/v6/97ec11893b9562fd36a60d8f/pair/USD/PEN"))
+                    .build();
+            HttpResponse<String> response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+            System.out.println(response.body());
+
+        Gson gson = new Gson();
+        
+
+        }catch(IOException | InterruptedException e){
+            System.out.println("Error " + e.getMessage());
+        }
+
+
+    }
+
+}
