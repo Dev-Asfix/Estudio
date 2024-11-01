6e426c4974f753b3f778f431f2b5b0b9dd56f663
Dev-Asfix <rosasramirez753@gmail.com>
2024-11-01 13:10:48 -0500

    sort

---
+++ Java/com/aluracursos/principal/Ejemplo.java
@@ -0,0 +1,18 @@
+package com.aluracursos.screenmatch.principal;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Ejemplo {
+
+    public void muestraEjemplo(){
+        List<String> nombres = Arrays.asList("Pablo", "Aldair", "Jack","Smit","Perla");
+        nombres.stream()
+                .sorted()
+                .limit(2)
+                .filter(e -> e.startsWith("A"))
+                .map(e -> e.toUpperCase())
+                .forEach(System.out::println);
+        ;
+    }
+}
