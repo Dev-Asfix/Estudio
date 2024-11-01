f64066f4fd16f32d6b0c6606f3af82bc958ee561
Dev-Asfix <rosasramirez753@gmail.com>
2024-10-31 20:41:05 -0500

    Last

--- Java/com/aluracursos/service/ConvierteDatos.java
+++ Java/com/aluracursos/service/ConvierteDatos.java
@@ -1,5 +1,20 @@
-package com.aluracursos.service;
+package com.aluracursos.screenmatch.service;
 
-public class ConvierteDatos {
-    
+
+import com.aluracursos.screenmatch.model.DatosSerie;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class ConvierteDatos implements IConvertirDatos{
+        private ObjectMapper objectMapper = new ObjectMapper();
+
+
+    @Override
+    public <T> T obtenerDatos(String json, Class<T> clase) {
+        try {
+            return objectMapper.readValue(json, clase);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
