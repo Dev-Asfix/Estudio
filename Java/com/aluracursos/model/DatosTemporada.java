3546c8a05f0158ef18b419b225d79fceb4a30c6c
Dev-Asfix <rosasramirez753@gmail.com>
2024-10-31 20:31:06 -0500

    tem2

--- Java/com/aluracursos/model/DatosTemporada.java
+++ Java/com/aluracursos/model/DatosTemporada.java
@@ -1,5 +1,16 @@
-package com.aluracursos.model;
+package com.aluracursos.screenmatch.model;
 
-public class DatosTemporada {
-    
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.List;
+
+
+@JsonIgnoreProperties(ignoreUnknown= true)
+
+public record DatosTemporada(
+        @JsonAlias("Season")Integer temporada,
+        @JsonAlias("Episodes") List<DatosEpisodio> episodios
+
+        ) {
 }
