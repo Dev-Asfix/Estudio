2e2e167410a92c6e8fe290e2dad867b2c388a8b6
Dev-Asfix <rosasramirez753@gmail.com>
2024-10-31 20:40:26 -0500

    serializacion

--- Java/com/aluracursos/model/DatosEpisodio.java
+++ Java/com/aluracursos/model/DatosEpisodio.java
@@ -1,5 +1,14 @@
-package com.aluracursos.model;
+package com.aluracursos.screenmatch.model;
 
-public class DatosEpisodio {
-    
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+
+
+public record DatosSerie(
+
+        @JsonAlias("Title") String titulo,
+        @JsonAlias("totalSeasons") Integer totalDeTemporadas,
+        @JsonAlias("imdbRating") String evaluacion){
 }
