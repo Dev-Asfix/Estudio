2aa20c0f0e1eba8736ddd1a9eb46b734440abef2
Dev-Asfix <rosasramirez753@gmail.com>
2024-11-04 20:07:56 -0500

    record

--- Java/com/alura/proyecto/src/main/java/com/alura/proyecto/model/Libro.java
+++ Java/com/alura/proyecto/src/main/java/com/alura/proyecto/model/Libro.java
@@ -3,11 +3,17 @@ package com.alura.proyecto.model;
 import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import java.util.List;
+
 @JsonIgnoreProperties(ignoreUnknown = true)
 
 public record Libro(
         @JsonAlias("id") Integer id,
         @JsonAlias("title") String titulo,
-        @JsonAlias("download_count") Integer descargas
+        @JsonAlias("download_count") double descargas,
+        @JsonAlias("authors") List<DatosAutor> autor,
+        @JsonAlias("languages") List<String> idiomas
+
+
 ) {
 }
