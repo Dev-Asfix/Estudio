0d24c95154f93837dd83551acf3ab5a85527f168
Dev-Asfix <rosasramirez753@gmail.com>
2024-11-07 22:18:05 -0500

    categoria

--- Java/com/alura/screenmatch/src/main/java/com/aluracursos/screenmatch/model/Categoria.java
+++ Java/com/alura/screenmatch/src/main/java/com/aluracursos/screenmatch/model/Categoria.java
@@ -5,7 +5,8 @@ public enum Categoria {
     ROMANCE("Romance"),
     COMEDIA("Comedy"),
     DRAMA("Drama"),
-    CRIMEN("Crimen");
+    CRIMEN("Crimen"),
+    AVENTURA("Adventure");
 
     private String categoriaOmdb;
         Categoria (String categoriaOmdb){
