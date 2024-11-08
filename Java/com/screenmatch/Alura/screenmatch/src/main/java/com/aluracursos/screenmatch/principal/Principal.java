9eb642546aa606f6399f531f53266dbf79d67cbd
Dev-Asfix <rosasramirez753@gmail.com>
2024-11-07 22:22:41 -0500

    variables

--- Java/com/screenmatch/Alura/screenmatch/src/main/java/com/aluracursos/screenmatch/principal/Principal.java
+++ Java/com/screenmatch/Alura/screenmatch/src/main/java/com/aluracursos/screenmatch/principal/Principal.java
@@ -14,6 +14,7 @@ import java.util.stream.Collectors;
 
 public class Principal {
 
+        //Variables
     private Scanner in = new Scanner(System.in);
     private ConsumoAPI consumoAPI = new ConsumoAPI();
     private final String API_KEY = "&apikey=ef162820";
