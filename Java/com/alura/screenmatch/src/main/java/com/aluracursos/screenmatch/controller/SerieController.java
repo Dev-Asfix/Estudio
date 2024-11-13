9fc29211669f862a7697e58ffb5998802f002e1b
Dev-Asfix <rosasramirez753@gmail.com>
2024-11-12 22:43:15 -0500

    controller

--- Java/com/alura/screenmatch/src/main/java/com/aluracursos/screenmatch/controller/SerieController.java
+++ Java/com/alura/screenmatch/src/main/java/com/aluracursos/screenmatch/controller/SerieController.java
@@ -4,24 +4,37 @@ package com.aluracursos.screenmatch.controller;
 import com.aluracursos.screenmatch.dto.SerieDTO;
 
 import com.aluracursos.screenmatch.repository.SerieRepository;
+import com.aluracursos.screenmatch.service.SerieService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
+@RequestMapping("/series")
 public class SerieController {
     @Autowired
-    private SerieRepository repository;
+    private SerieService service;
 
-    @GetMapping("/series")
+    @GetMapping()
     public List<SerieDTO> mostrarSeries(){
-        return repository.findAll()
-                .stream()
-                .map(s -> new SerieDTO(s.getId(), s.getTitulo(),s.getTotalTemporadas(),s.getEvaluacion(),s.getPoster(),s.getGenero(),s.getActores(),s.getSinopsis()))
-                .collect(Collectors.toList());
+        return service.obtenerSeries();
+    }
+
+    @GetMapping("/top5")
+    public List<SerieDTO> top5Series(){
+        return service.top5Series();
+    }
+
+    @GetMapping("/lanzamientos")
+    public List<SerieDTO> lanzamientos(){
+        return service.lanzamientosRecientes();
+    }
+
+    @GetMapping("/{id}")
+    public SerieDTO obtenerId(@PathVariable Long id){
+        return service.obtenerId(id);
     }
 
 }
