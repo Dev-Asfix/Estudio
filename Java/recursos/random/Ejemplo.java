95c2a96743090c3e098520cc1baee107329d5302
Dev-Asfix <rosasramirez753@gmail.com>
2024-11-13 21:23:15 -0500

    example

---
+++ Java/recursos/random/Ejemplo.java
@@ -0,0 +1,12 @@
+public class Ejemplo {
+  public static void main(String args[]) {
+	
+      int edad = 15;
+
+      if(edad >= 18){
+	  System.out.println("Eres Mayor");
+      } else {
+    	  System.out.println("Eres Menor");
+      }
+  }
+}
