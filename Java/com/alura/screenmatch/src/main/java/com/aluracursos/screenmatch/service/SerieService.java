6e3b98ba73e4cd220e4eda85794e105062627367
Dev-Asfix <rosasramirez753@gmail.com>
2024-11-14 22:19:26 -0500

    record

--- Java/com/alura/screenmatch/src/main/java/com/aluracursos/screenmatch/service/SerieService.java
+++ Java/com/alura/screenmatch/src/main/java/com/aluracursos/screenmatch/service/SerieService.java
@@ -1,6 +1,8 @@
 package com.aluracursos.screenmatch.service;
 
+import com.aluracursos.screenmatch.dto.EpisodioDTO;
 import com.aluracursos.screenmatch.dto.SerieDTO;
+import com.aluracursos.screenmatch.model.Categoria;
 import com.aluracursos.screenmatch.model.Serie;
 import com.aluracursos.screenmatch.repository.SerieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -42,6 +44,32 @@ public class SerieService {
                     s.getGenero(),s.getActores(),s.getSinopsis());
         }
         return null;
+    }
+
+    public List<EpisodioDTO> obtenerTemporadas(Long id){
+        Optional<Serie> serie = repository.findById(id);
+        if(serie.isPresent()){
+            Serie s = serie.get();
+            return s.getEpisodios().stream()
+                    .map(e -> new EpisodioDTO(e.getTemporada(),e.getTitulo(),
+                            e.getNumeroEpisodio()))
+                    .collect(Collectors.toList());
+
+        }
+        return null;
+
+    }
+
+    public List<EpisodioDTO> obtenerTemporadaPorId(Long id, Long idTemporada){
+        return repository.obtenerTemporadaPorId(id, idTemporada).stream()
+                .map(e -> new EpisodioDTO(e.getTemporada(),e.getTitulo(),e.getNumeroEpisodio()))
+                .collect(Collectors.toList());
+    }
+
+
+    public List<SerieDTO> obtenerSerieCategoria(String nombreCategoria){
+        Categoria categoria = Categoria.fromEspanol(nombreCategoria);
+        return convierteDatos(repository.findByGenero(categoria));
     }
 
 
