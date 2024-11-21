5e5975b312f6b5587058e550dcd67874c1d5d52a
Dev-Asfix <rosasramirez753@gmail.com>
2024-11-21 17:35:38 -0500

    rest

---
+++ Java/med/voll/api/src/main/java/med/voll/api/controller/ControllerHello.java
@@ -0,0 +1,31 @@
+package med.voll.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+@RestController
+@RequestMapping("ruta")
+public class ControllerHello {
+
+    @GetMapping("/queso")
+    public int helloworld(){
+        return 54;
+    }
+
+    @GetMapping("/ejemplo")
+    public List<Object> hola(){
+        List<Object> hol = new ArrayList<>();
+
+        hol.add("hola");
+        hol.add("Gato");
+        hol.add(23);
+        hol.add();
+        return hol;
+    }
+
+}
