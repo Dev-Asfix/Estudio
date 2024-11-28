104e626d08477e85b6cc7bb0236049448faf0201
Dev Asfix <rosasramirez753@gmail.com>
2024-11-27 21:20:39 -0500

    Direccion

--- Java/med/voll/api/src/main/java/med/voll/api/direccion/DatosDireccion.java
+++ Java/med/voll/api/src/main/java/med/voll/api/direccion/DatosDireccion.java
@@ -1,12 +1,18 @@
 package med.voll.api.direccion;
 
+import jakarta.validation.constraints.NotBlank;
 import med.voll.api.medico.Especialidad;
 
 public record DatosDireccion(
+        @NotBlank
         String calle,
+        @NotBlank
         String distrito,
+        @NotBlank
         String ciudad,
+        @NotBlank
         String numero,
+        @NotBlank
         String complemento
         ) {
 
