e765b4a81580182e3a06c0aafdc4026d04c4804f
Dev Asfix <rosasramirez753@gmail.com>
2024-11-27 21:20:31 -0500

    controller

--- Java/med/voll/api/src/main/java/med/voll/api/controller/MedicoController.java
+++ Java/med/voll/api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,6 +1,11 @@
 package med.voll.api.controller;
 
+import jakarta.validation.Valid;
+import lombok.Value;
 import med.voll.api.medico.DatosRigistroMedico;
+import med.voll.api.medico.Medico;
+import med.voll.api.medico.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +15,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/medicos")
 public class MedicoController {
 
+    //Ojo que no es recomendable para testing , porque a lo declaramos a nivel de repository , y es mejor con setter
+
+    @Autowired
+    private MedicoRepository repository;
+
     @PostMapping
-    public void registrar(@RequestBody DatosRigistroMedico datos){
+    public void registrar(@RequestBody @Valid DatosRigistroMedico datos){
+
         System.out.println("datos recibidos " + datos);
+        repository.save(new Medico(datos));
     }
 }
