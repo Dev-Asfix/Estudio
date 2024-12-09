be1ddc9cbda9dc43baa991fc5e04097dc528307c
Dev Asfix <rosasramirez753@gmail.com>
2024-12-08 20:38:48 -0500

    commit

--- 7DaysOfCode/Fizzbuzz.js
+++ 7DaysOfCode/Fizzbuzz.js
@@ -1,13 +1,14 @@
 
-for(let i = 1; i<=100;i++){
-    if(i%3 === 0 && i%5===0){
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
         console.log('FizzBuz')
-    } else if(i%3 ===0){
+    } else if (i % 3 === 0) {
         console.log('Bizz')
-    } else if(i%5 ===0){
+    } else if (i % 5 === 0) {
         console.log('buzz')
     } else {
         console.log(i);
+        //hello world 
     }
-    
+
 }
