1faa83308162a90ceb0585adbaaabada54ded876
Dev Asfix <rosasramirez753@gmail.com>
2024-12-20 23:56:34 -0500

    controller

---
+++ Java/med/voll/api/src/main/java/med/voll/api/controller/ConsultaController.java
@@ -0,0 +1,36 @@
+package med.voll.api.controller;
+
+import jakarta.validation.Valid;
+import med.voll.api.domain.consulta.validaciones.cancelamiento.DatosCancelamientoConsulta;
+import med.voll.api.domain.consulta.DatosReservaConsulta;
+import med.voll.api.domain.consulta.ReservaDeConsultas;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/consultas")
+public class ConsultaController {
+
+    @Autowired
+    private ReservaDeConsultas reserva;
+
+    @PostMapping
+    @Transactional
+    public ResponseEntity reservar(@RequestBody @Valid DatosReservaConsulta datos){
+        System.out.println(datos);
+
+        var detalleConsulta = reserva.reservar(datos);
+
+
+        return ResponseEntity.ok(detalleConsulta);
+    }
+
+    @DeleteMapping
+    @Transactional
+    public ResponseEntity cancelar(@RequestBody @Valid DatosCancelamientoConsulta datos) {
+        reserva.cancelar(datos);
+        return ResponseEntity.noContent().build();
+    }
+}
