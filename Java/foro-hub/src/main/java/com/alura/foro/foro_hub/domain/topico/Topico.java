bba02cdf86764493f56eb3ebca72b6ce8ae61d85
Dev Asfix <rosasramirez753@gmail.com>
2024-12-28 21:14:11 -0500

    class

--- Java/foro-hub/src/main/java/com/alura/foro/foro_hub/domain/topico/Topico.java
+++ Java/foro-hub/src/main/java/com/alura/foro/foro_hub/domain/topico/Topico.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 @Table(name = "topicos")
 @Getter
 @Setter
-@NoArgsConstructor
+//@NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(of = "id")
 public class Topico {
@@ -23,7 +23,8 @@ public class Topico {
     private String titulo;
     private String mensaje;
     private LocalDateTime fecha_creacion;
-    private String status;
+    @Enumerated(EnumType.STRING)
+    private Estados status;
 
     @ManyToOne
     @JoinColumn(name = "autor_id")
@@ -33,5 +34,72 @@ public class Topico {
     @JoinColumn(name = "curso_id")
     private Curso curso;
 
+    public Topico(){
 
+    }
+
+    public Topico(DatosRegistroTopico datos, Usuario autor, Curso curso){
+        this.titulo = datos.titulo();
+        this.mensaje = datos.mensaje();
+        this.fecha_creacion = LocalDateTime.now();
+        this.status = Estados.ABIERTO;
+        this.autor = autor;
+        this.curso = curso;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+
+    public LocalDateTime getFecha_creacion() {
+        return fecha_creacion;
+    }
+
+    public void setFecha_creacion(LocalDateTime fecha_creacion) {
+        this.fecha_creacion = fecha_creacion;
+    }
+
+    public Estados getStatus() {
+        return status;
+    }
+
+    public void setStatus(Estados status) {
+        this.status = status;
+    }
+
+    public Usuario getAutor() {
+        return autor;
+    }
+
+    public void setAutor(Usuario autor) {
+        this.autor = autor;
+    }
+
+    public Curso getCurso() {
+        return curso;
+    }
+
+    public void setCurso(Curso curso) {
+        this.curso = curso;
+    }
 }
