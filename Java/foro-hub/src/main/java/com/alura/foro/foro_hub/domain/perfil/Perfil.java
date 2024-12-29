729dd117c1f764053b256ccbd3d44d14c0e196bf
Dev Asfix <rosasramirez753@gmail.com>
2024-12-28 21:14:17 -0500

    class

--- Java/foro-hub/src/main/java/com/alura/foro/foro_hub/domain/perfil/Perfil.java
+++ Java/foro-hub/src/main/java/com/alura/foro/foro_hub/domain/perfil/Perfil.java
@@ -2,16 +2,24 @@ package com.alura.foro.foro_hub.domain.perfil;
 
 import jakarta.persistence.*;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
+
+import java.util.Optional;
 
 @Entity(name = "perfil")
 @Table(name = "perfiles")
 @Getter
 @Setter
-@NoArgsConstructor
+//@NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(of = "id")
 public class Perfil {
+
+    public Perfil(){
+
+    }
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,6 +30,7 @@ public class Perfil {
     public Perfil(@Valid DatosRegistroPerfil datos) {
         this.nombre = datos.nombre();
     }
+
 
     public Long getId() {
         return id;
