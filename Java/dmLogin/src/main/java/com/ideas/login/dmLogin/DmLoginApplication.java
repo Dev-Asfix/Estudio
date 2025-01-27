73fdd81e74fd33e496592b9d7fbe25e1d94a78ab
Dev Asfix <rosasramirez753@gmail.com>
2025-01-27 11:17:15 -0500

    Security v2

---
+++ Java/dmLogin/src/main/java/com/ideas/login/dmLogin/DmLoginApplication.java
@@ -0,0 +1,13 @@
+package com.ideas.login.dmLogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DmLoginApplication.class, args);
+	}
+
+}
---
+++ Java/dmLogin/src/test/java/com/ideas/login/dmLogin/DmLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ideas.login.dmLogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
