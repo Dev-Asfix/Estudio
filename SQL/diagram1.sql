234558c989366da8120e82d2c78efb98f20815dc
Dev Asfix <rosasramirez753@gmail.com>
2025-02-02 14:41:33 -0500

    SQL

---
+++ SQL/diagram1.sql
@@ -0,0 +1,8 @@
+use ventas_jugos;
+
+insert into tb_productos VALUES
+('10401107', 'Light', 'SAndia', '350 ml', 'Lata', '4.56'),
+('1040108', 'Light', 'Asai', '350 ml', 'Lata', '4.56'),
+('1040109', 'Light', 'Manzana', '35 ml', 'Lata', '4.56'),
+('1040110', 'Light', 'Mango', '50 ml', 'Lata', '4.56');
+
