990b9fefe5a00f5483d13d8ef7889eaea28afe31
Dev Asfix <rosasramirez753@gmail.com>
2025-02-23 11:16:19 -0500

    Proyect migration to Java

---
+++ Java/Proyect/floatbin/src/main/java/com/dev/floatbin/FloatbinApplication.java
@@ -0,0 +1,13 @@
+package com.dev.floatbin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FloatbinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FloatbinApplication.class, args);
+	}
+
+}
---
+++ Java/Proyect/floatbin/src/main/java/com/dev/floatbin/config/WebSocketConfig.java
@@ -0,0 +1,4 @@
+package com.dev.floatbin.config;
+
+public class WebSocketConfig {
+}
