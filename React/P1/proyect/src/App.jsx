ec2e4ba0eaf5fbb3f1920ee46a71df93c2cdbb9a
Dev Asfix <rosasramirez753@gmail.com>
2025-03-24 00:25:01 -0500

    React v2

---
+++ React/P1/proyect/src/App.jsx
@@ -0,0 +1,16 @@
+import Header from "./components/Header";
+
+function App(){
+return(
+  <>
+  
+  <Header />
+    <main className="p-4">
+      <p>Bienvenido a mi sitio web en reactr.</p>
+    </main>
+
+  </>
+);
+}
+
+export default App;
---
+++ React/P1/proyect/src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+    return (
+      <header className="bg-blue-500 text-white p-4 text-center">
+        <h1>Mi Página Web</h1>
+      </header>
+    );
+  }
+  export default Header;
+  
